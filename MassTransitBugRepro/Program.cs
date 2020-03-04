8053ba6062c6f0272dc2abf5819f59f40864e79b
Stephen Lomangino <slomangino@flightdocs.com>
2020-03-04 15:59:42 -0500

    initial commit

---
+++ MassTransitBugRepro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GreenPipes;
+using MassTransit;
+using MassTransit.PublishPipeSpecifications;
+using Microsoft.ApplicationInsights;
+
+namespace MassTransitBugRepro
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var bus = Bus.Factory.CreateUsingInMemory(sbc =>
+			{
+				var host = sbc.Host(h =>
+				{
+				});
+
+				sbc.ReceiveEndpoint("my_queue", endpoint =>
+				{
+					endpoint.Handler<MyMessage>(async context =>
+					{
+						await Console.Out.WriteLineAsync($"Received: {context.Message.Value}");
+					});
+				});
+
+				sbc.UseApplicationInsightsOnPublish(new TelemetryClient());
+			});
+
+			await bus.StartAsync();
+
+			await bus.Publish(new MyMessage { Value = "Hello, World." });
+
+			Console.ReadLine();
+
+			await bus.StopAsync();
+		}
+
+    }
+
+	class MyMessage
+	{
+		public string Value { get; set; }
+	}
+}
